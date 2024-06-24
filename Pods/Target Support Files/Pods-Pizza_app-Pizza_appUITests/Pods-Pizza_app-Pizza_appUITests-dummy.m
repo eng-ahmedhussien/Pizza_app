c345d031d5cf00a36c1666a86326d4233b058e67
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2024-06-24 10:28:00 +0300

    modify slidercell cconstrains

---
+++ Pods/Target Support Files/Pods-Pizza_app-Pizza_appUITests/Pods-Pizza_app-Pizza_appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pizza_app_Pizza_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pizza_app_Pizza_appUITests
+@end
---
+++ Pods/Target Support Files/Pods-Pizza_appTests/Pods-Pizza_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pizza_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_Pizza_appTests
+@end
